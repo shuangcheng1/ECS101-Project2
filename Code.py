69aeab785401df89dde36cdfd01de02643e979c1
SophiaLuo50623 <sluo@sjkeagles.com>
2022-11-08 10:39:09 -0500

    New Commit

--- Code.py
+++ Code.py
@@ -48,15 +48,37 @@ def exploreOnly():
     print(happiness)
 
 def eGreedy(e = 10):
+    cafeterias = []
+    happiness = 0
+
     cafeteria1 = random.normalvariate(10, 8)
     cafeteria2 = random.normalvariate(15, 6)
-    cafeteria3 = random.normalvariate(10, 8)
+    cafeteria3 = random.normalvariate(12, 5)
+
+
+    cafeterias.append(cafeteria1)
+    cafeterias.append(cafeteria2)
+    cafeterias.append(cafeteria3)
+
+    best = max(cafeterias)
 
 
     r = random.random()
     if r < (e/100):
         i = random.randint(1,3)
+        if i == 1:
+            happiness += random.normalvariate(10, 8)
+        elif i == 2:
+            happiness += random.normalvariate(15, 6)
+        else:
+            happiness += random.normalvariate(12, 5)
 
+
+    else:
+        happiness += best
+
+
+    return happiness
 
 
 
